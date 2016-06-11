0e2ec3c4e758f525f9766dfa669597222cdd73ca
david roe <rdr@robertdavidroe.com>
2016-06-11 13:18:55 -0500

    Comments and very minor miscellaneous tweaks

--- Song.js
+++ Song.js
@@ -1,30 +1,45 @@
+/**
+ * Model file for raw song data
+ */
 
 var bb = require('backbone'),
     parser = require('note-parser');
 
-
-
-
-
-
 var Song = bb.Model.extend(
 {
+    /**
+     * Backbone model defaults
+     *
+     * @type {Object}
+     */
+    defaults: {
+        title: null,
+        phases: new Array(), //array of Phase objects; parts of the song such as verse, chorus, and bridge.
+        writeablePhases: new Array(), //deprecated; vestige of the first version of the code
+    },
+    /**
+     * Set up the instance
+     * @param  {object} attribs Backbone model attributes
+     * @param  {object} opts    Options (arguments, basically) for this function
+     *
+     * @return void
+     */
     initialize: function(attribs, opts){
         var that = this;
         this.phaseMeta = [];
         this.writeableDuration = opts.writeableDuration;
         this.hist = [];
-        // if (this.get('phases')) {
-        //     this.set('phases', []);
-        // }
+
     },
-    defaults: {
-        title: null,
-        phases: new Array(), //array of Phase objects
-        writeablePhases: new Array(), //array of Phase objects
-    },
+    /**
+     * Return a phase by name
+     *
+     * @param  {string} pn phase name
+     *
+     * @return object/false   Requested phrase
+     */
     getPhase: function(pn){
-        console.log('getPhaes');
+
         if (pn
             && this.get
             && this.attributes
@@ -36,15 +51,29 @@ var Song = bb.Model.extend(
                 return phs;
             }
         } else {
-            console.log('36', this.attributes);
             return false;
         }
     },
+    /**
+     * Pass a phase name, function, and args for the function
+     * to call this function and operate it on a phase of the
+     * song.
+     *
+     * @param  {[type]}   phsNm  [description]
+     * @param  {Function} fn     [description]
+     * @param  {[type]}   params [description]
+     * @return {[type]}          [description]
+     */
     portal: function(phsNm, fn, params){
 
-        return this.getPhase(phsNm);
+        return fn(this.getPhase(phsNm));
 
     },
+    /**
+     * Calculate the number of phases in the song
+     *
+     * @return {Number} Lengh value of this.attributes.phases
+     */
     countPhases: function(){
 
         var phss = this.attributes.phases;
@@ -52,6 +81,16 @@ var Song = bb.Model.extend(
         return (phss && (phss.length !== undefined && phss.length !== null)) ? phss.length : undefined;
 
     },
+    /**
+     * Add a phase to the song
+     *
+     * @param  {object} phase  Phase data
+     * @param  {object} pegLib Arpeggio-making map
+     * @param  {string} nm     key name for phase
+     * @param  {number} idx    index at which to assign phase
+     *
+     * @return {undefined}
+     */
     addPhase: function(phase, pegLib, nm, idx)
     {
         if (typeof phase === 'object') {
@@ -67,6 +106,12 @@ var Song = bb.Model.extend(
         }
 
     },
+    /**
+     * Get the contents of the song bar by bar--for playing as
+     * opposed to saving to a file.
+     *
+     * @return {array} bars of the song
+     */
     readBars: function()
     {
         var retVar = [],
@@ -83,6 +128,13 @@ var Song = bb.Model.extend(
         _.each(this.attributes.phases, forEachPhase);
         return retVar;
     },
+    /**
+     * "Sheet music" means a spare notation that can be
+     * converted to binary and sent down to the user as a
+     * MIDI file (as opposed to playing in browser).
+     *
+     * @return {array} Array of notes
+     */
     getSheetMusicBars: function()
     {
         var retVar = [],
@@ -102,6 +154,15 @@ var Song = bb.Model.extend(
         _.each(this.attributes.phases, forEachWrPhase);
         return retVar;
     },
+    /**
+     * Return notes of the bar in a reduced form readily convertable
+     * to binary (as opposed to playing in browser).
+     *
+     * @param  {array}  bar    array of notes
+     * @param  {number} phsIdx index from which the bar originated
+     *
+     * @return {array} Array of music notes in spare notation
+     */
     formatWriteableBar: function(bar, phsIdx)
     {
         var that = this,
@@ -115,6 +176,15 @@ var Song = bb.Model.extend(
         return retVar;
 
     },
+    /**
+     * Format a note to be placed into a writeable bar.
+     *
+     * @param  {string} nt       tonal.js style note info
+     * @param  {number} ntIdx    index of note relative to the bar
+     * @param  {number} phaseIdx index of the phase from which this note originated
+     *
+     * @return {object}          writeable note data
+     */
     formatWriteableNote: function(nt, ntIdx, phaseIdx)
     {
         var pc = parser.parse(nt),
@@ -126,6 +196,14 @@ var Song = bb.Model.extend(
             };
         return writeData;
     },
+    /**
+     * Utility-type function for formatting a bar for consumption
+     * by the client
+     *
+     * @param  {array} bar  Array of notes
+     *
+     * @return {object}     object wrapped how the client expects it
+     */
     formatOutputBar: function(bar)
     {
         return {chord: bar};
